c494e739d840bb2a4fe6c895e90c94c6b222ba2d
Vitor-labs <v02hx10@gmail.com>
2023-07-10 06:42:21 -0300

    project initialization

--- src/drivers/interfaces/html_collector.py
+++ src/drivers/interfaces/html_collector.py
@@ -1,7 +1,31 @@
+"""Interface to implement HTML COLLECTOR methods
+
+Raises:
+    NotImplementedError: method "collect_info" not implemented
+"""
 from abc import ABC, abstractmethod
 from typing import List, Dict
 
 class HtmlCollectorInterface(ABC):
+    """Interface that defines the HTML COLLECTOR methods
+
+    Args:
+        ABC (ABC): Abstract Class 
+
+    Raises:
+        NotImplementedError: method "collect_info" not implemented
+    """
     @abstractmethod
-    def collect_info(cls, html:str)->List[Dict[str,str]]:
+    def collect_info(self, html:str) -> List[Dict[str, str]]:
+        """Collects Data from destination
+
+        Args:
+            html (str): _description_
+
+        Raises:
+            NotImplementedError: method "collect_info" not implemented
+
+        Returns:
+            List[Dict[str, str]]: _description_
+        """
         raise NotImplementedError("HtmlCollector.collect_info: not implemented")
